b32f207e2107755dbdba22b68080953a8baac981
T.makino <makimaki960@gmail.com>
2021-03-28 18:35:46 +0900

    use React.Fragment to render 2Line tags

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 
 function App() {
-  // const greeting = "Hi!!!"
-  // const dom = <h1 className="foo">{greeting}</h1>
-  const dom = <input type="text" onChange={() => console.log("I am cliked.")} />;
-  return dom;
+  return(
+    <React.Fragment>
+      <label htmlFor="bar">bar</label>
+      <input type="text" onChange={()=> console.log("I am clicked!!")}/>
+    </React.Fragment>
+  )
 }
 
 export default App;
